7d05c102dd81b5cdfc68813dc29ee41b048752b7
Sebastian Rojo <sebastianrojocollao@gmail.com>
2021-06-12 18:00:18 -0400

    Feat: Crea repositorio con dependencias

---
+++ src/main/java/com/sebastianrojo/empges/EmpgesApplication.java
@@ -0,0 +1,13 @@
+package com.sebastianrojo.empges;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmpgesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EmpgesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sebastianrojo/empges/EmpgesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sebastianrojo.empges;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpgesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
